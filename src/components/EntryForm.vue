3b185abddd775f4a7fe1018ab0ea569199f534e2
Chris Black <black@chrisblack.net>
2019-03-26 13:41:18 -0700

    Update EntryForm.vue
    Added blur() when place/category selected to make dropdown go away.

--- src/components/EntryForm.vue
+++ src/components/EntryForm.vue
@@ -7,14 +7,14 @@
 
         <input type="number" step="0.01" placeholder="0.00" v-model="amount">
 
-        <input type="text" list="prevPlaces" placeholder="At this place" class="dropdown" v-model="place">
+        <input type="text" list="prevPlaces" placeholder="At this place" class="dropdown" v-model="place" @click="place = ''" @change="onSelect">
         <datalist ID="prevPlaces">
             <option v-for="place in places" :key="place.place" :value="place.place"></option>
         </datalist>
 
         <textarea placeholder="Notes" v-model="notes"></textarea>
 
-        <input type="text" list="prevCats" placeholder="Category" class="dropdown" v-model="category">
+        <input type="text" list="prevCats" placeholder="Category" class="dropdown" v-model="category" @click="category = ''" @change="onSelect">
         <datalist ID="prevCats">
             <option v-for="(cat, key) in categories" :key="key" :value="cat"></option>
         </datalist>
@@ -62,6 +62,9 @@
             },
             emitClick () {
                 this.$emit('itemadded');
+            },
+            onSelect () {
+                document.activeElement.blur();
             }
         }
     }
